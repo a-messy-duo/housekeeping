e2381b8aa8411a882271f2b21ddf2b5c0971e91e
anyl92 <yulim9592@gmail.com>
2023-04-29 18:21:09 +0900

    feat: bottom sheet UI

--- src/features/shared/bottomSheet/Container.tsx
+++ src/features/shared/bottomSheet/Container.tsx
@@ -1,7 +1,49 @@
 import { FunctionComponent } from 'react';
 
+import './bottomSheet.scss';
+
 const BottomSheetContainer: FunctionComponent = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="itemBox">
+        <div className="item">
+          <div className="itemTitle">이름</div>
+          <fieldset className="itemInput itemRadio">
+            <div className="itemRadioBox">
+              <input type="radio" id="nameY" name="name" value="Y" checked />
+              <label htmlFor="nameY" className="addMarginRight">
+                Y
+              </label>
+
+              <input type="radio" id="nameC" name="name" value="C" />
+              <label htmlFor="nameC">C</label>
+            </div>
+          </fieldset>
+        </div>
+        <div className="item">
+          <div className="itemTitle">날짜</div>
+          <input className="itemInput" type="date" />
+        </div>
+        <div className="item">
+          <div className="itemTitle">구분</div>
+          <input className="itemInput" type="text" />
+        </div>
+        <div className="item">
+          <div className="itemTitle">내역</div>
+          <input className="itemInput" type="text" />
+        </div>
+        <div className="item">
+          <div className="itemTitle">금액</div>
+          <input className="itemInput" type="number" />
+        </div>
+        <div className="item">
+          <div className="itemTitle">메모</div>
+          <input className="itemInput" type="text" />
+        </div>
+      </div>
+      <button className="addBtn">추가</button>
+    </div>
+  );
 };
 
 export default BottomSheetContainer;
